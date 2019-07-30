49d33726abc0196ceeb240bc535f7a2dc779d048
crdbyb <crdbyb@example.com>
2019-07-30 20:50:52 +0800

    .

---
+++ cwcms/src/service/image/LogicException.java
@@ -0,0 +1,23 @@
+package com.cwcms.service.image;
+
+
+//业务逻辑异常
+public class LogicException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 * @param message	异常信息
+	 * @param cause		异常的根本原因
+	 */
+	public LogicException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public LogicException(String message) {
+		super(message);
+	}
+
+	
+}
